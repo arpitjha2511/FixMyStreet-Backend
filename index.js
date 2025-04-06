e5d608b0f4b3c5b2e7cf20ab8636831cbd7fe58d
Arpit Jha <arpitjha2511@gmail.com>
2025-04-06 17:43:27 +0530

    final project

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const axios = require('axios'); // For making external API requests
 const app = express();
 const port = 3000;
 const cors = require('cors');
-//ngrok startup command: 
+// ngrok startup command: 
 // ngrok http --url=wildcat-mint-actually.ngrok-free.app 3000
 // Increase limit for JSON payload
-app.use(express.json({ limit: '100mb' }));
-app.use(express.urlencoded({ limit: '100mb', extended: true }));
+app.use(express.json({ limit: '200mb' }));
+app.use(express.urlencoded({ limit: '200mb', extended: true }));
 app.use(cors());
 
 app.use((req, res, next) => {
@@ -107,23 +107,25 @@ app.post('/uploadImage', async (req, res) => {
 
         // Save the initial pothole entry to get the unique ID
         const savedPothole = await newPothole.save();
-
+        console.log(` New Pothole saved with id: ${savedPothole._id.toString()}`);
         // Send the image and image_id to the external API
         const response = await axios.post('https://touching-subtle-bat.ngrok-free.app/detect_potholes', {
             image,
             id: savedPothole._id.toString()
         }, 
         { timeout: 60000 }
-    );
+        );
 
         const { image: updatedImage, severity } = response.data;
-
+        // make sure it's a Number
+        const severityNum = parseInt(severity, 10);     
+        const finalSeverity = severityNum + 2;
         // Ensure updatedImage has the prefix before saving
         const finalImage = updatedImage.startsWith(base64Prefix) ? updatedImage : base64Prefix + updatedImage;
-
+        
         // Update the saved pothole with the processed image and severity level
         savedPothole.image = finalImage;
-        savedPothole.threat = severity;
+        savedPothole.threat = finalSeverity;
         await savedPothole.save();
 
         res.status(201).json({ id: savedPothole._id });
